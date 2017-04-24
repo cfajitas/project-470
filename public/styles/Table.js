642f3decd00b126f682a2dc186b2120d0440d2e4
Cory Fleitas <cfajitas@gmail.com>
2017-04-24 23:09:07 +0000

    Adding Test Table

---
+++ public/styles/Table.js
@@ -0,0 +1,43 @@
+var url='http://localhost:8983/solr/gettingstarted/select?indent=on&q=akron&wt=json';
+var myList = $.getJSON(url);
+
+
+//var myList=[{"name" : "abc", "age" : 50},
+//            {"age" : "25", "hobby" : "swimming"},
+//            {"name" : "xyz", "hobby" : "programming"}];
+
+
+ function buildHtmlTable() {
+     var columns = addAllColumnHeaders(myList);
+ 
+     for (var i = 0 ; i < myList.length ; i++) {
+         var row$ = $('<tr/>');
+         for (var colIndex = 0 ; colIndex < columns.length ; colIndex++) {
+             var cellValue = myList[i][columns[colIndex]];
+ 
+             if (cellValue == null) { cellValue = ""; }
+ 
+             row$.append($('<td/>').html(cellValue));
+         }
+         $("#excelDataTable").append(row$);
+     }
+ }
+ 
+ function addAllColumnHeaders(myList)
+ {
+     var columnSet = [];
+     var headerTr$ = $('<tr/>');
+ 
+     for (var i = 0 ; i < myList.length ; i++) {
+         var rowHash = myList[i];
+         for (var key in rowHash) {
+             if ($.inArray(key, columnSet) == -1){
+                 columnSet.push(key);
+                 headerTr$.append($('<th/>').html(key));
+             }
+         }
+     }
+     $("#excelDataTable").append(headerTr$);
+ 
+     return columnSet;
+ }
