08680430ba3cc62fd13af65cccc7d6dbd0e6b3e1
Cory Fleitas <cfajitas@gmail.com>
2017-04-25 00:22:23 +0000

    Testing tables

--- public/styles/Table.js
+++ public/styles/Table.js
@@ -1,10 +1,10 @@
-var url='http://localhost:8983/solr/gettingstarted/select?indent=on&q=akron&wt=json';
-var myList = $.getJSON(url);
+var url = 'http://localhost:8983/solr/gettingstarted/select?indent=on&q=%2BFitnessCenter:1+%2Bsfbay&wt=json';
+//var myList = $.getJSON(url);
 
 
-//var myList=[{"name" : "abc", "age" : 50},
-//            {"age" : "25", "hobby" : "swimming"},
-//            {"name" : "xyz", "hobby" : "programming"}];
+var myList=[{"name" : "abc", "age" : 50},
+            {"age" : "25", "hobby" : "swimming"},
+            {"name" : "xyz", "hobby" : "programming"}];
 
 
  function buildHtmlTable() {
@@ -41,3 +41,28 @@ var myList = $.getJSON(url);
  
      return columnSet;
  }
+ 
+ 
+ $.getJSON(url,
+function (data) {
+    var tr;
+    for (var i = 0; i < data.length; i++) {
+        tr = $('<tr/>');
+        tr.append("<td>" + data[i].User_Name + "</td>");
+        tr.append("<td>" + data[i].score + "</td>");
+        tr.append("<td>" + data[i].team + "</td>");
+        $('jsonDataTable2').append(tr);
+    }
+});
+
+$(document).ready(function () {
+        var json = [{"User_Name":"John Doe","score":"10","team":"1"},{"User_Name":"Jane Smith","score":"15","team":"2"},{"User_Name":"Chuck Berry","score":"12","team":"2"}];
+        var tr;
+        for (var i = 0; i < json.length; i++) {
+            tr = $('<tr/>');
+            tr.append("<td>" + json[i].User_Name + "</td>");
+            tr.append("<td>" + json[i].score + "</td>");
+            tr.append("<td>" + json[i].team + "</td>");
+            $('jsonDataTable2').append(tr);
+        }
+});
